fb1b721b330e8b5f6f81f8b83756210d10ae8b7d
Oliver Metz <softwaredevdad@gmail.com>
2026-04-26 14:52:15 +0200

    Initial Project Setup.

---
+++ src/test/java/com/devdad/Forma/FormaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devdad.Forma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
