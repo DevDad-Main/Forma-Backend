f030b3ccff559e044c6dfa22d9cd1fac16a1d48a
Oliver Metz <softwaredevdad@gmail.com>
2026-05-07 16:38:44 +0200

    fix: Get Products Route 403 issue.
    - fix the 403 forbidden issue due to removing the auth check annotation
    on the controller but not removing the Security auth check in the
    service method, for now commented out until certain it needs to be
    completely eliminated, now all users should be able to see the products.

--- src/main/java/com/devdad/Forma/service/ProductService.java
+++ src/main/java/com/devdad/Forma/service/ProductService.java
@@ -20,51 +20,54 @@ import com.devdad.Forma.repository.ProductRepository;
 @Service
 public class ProductService {
 
-    @Autowired
-    private ProductRepository productRepository;
+	@Autowired
+	private ProductRepository productRepository;
 
-    public Product createProduct(Product product) {
-        return productRepository.save(product);
-    }
+	public Product createProduct(Product product) {
+		return productRepository.save(product);
+	}
 
-    public List<Product> getProducts() {
-        // Check if current user is admin (Alternative to
-        // @PreAuthorize("hasAuthority('ADMIN')"))
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        if (auth == null || !auth.getAuthorities().contains(new SimpleGrantedAuthority("ADMIN"))) {
-            throw new AccessDeniedException("Admin access required.");
-        }
+	public List<Product> getProducts() {
+		// TODO: Refactor - commented out as this should be a public route but on the
+		// deployed backend we get 401, 403 due to this which i missed before.
+		// Check if current user is admin (Alternative to
+		// @PreAuthorize("hasAuthority('ADMIN')"))
+		// Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		// if (auth == null || !auth.getAuthorities().contains(new
+		// SimpleGrantedAuthority("ADMIN"))) {
+		// throw new AccessDeniedException("Admin access required.");
+		// }
 
-        return productRepository.findAll();
-    }
+		return productRepository.findAll();
+	}
 
-    public List<Product> saveProducts(List<Product> products) {
-        for (Product product : products) {
-            // Clear ID so DB gernerates new ones.
-            // Due to frontend sending ids and merge issues
-            product.setId(0);
-        }
-        return productRepository.saveAll(products);
-    }
+	public List<Product> saveProducts(List<Product> products) {
+		for (Product product : products) {
+			// Clear ID so DB gernerates new ones.
+			// Due to frontend sending ids and merge issues
+			product.setId(0);
+		}
+		return productRepository.saveAll(products);
+	}
 
-    public Product updateProduct(int id, Product product) {
-        Product existingProduct = productRepository
-                .findById(id)
-                .orElseGet(() -> createProduct(product));
+	public Product updateProduct(int id, Product product) {
+		Product existingProduct = productRepository
+				.findById(id)
+				.orElseGet(() -> createProduct(product));
 
-        // copies all fields from 'product' to 'existingProduct'
-        // We can specify fields at the end to ignore.
-        BeanUtils.copyProperties(product, existingProduct, "id");
-        return productRepository.save(existingProduct);
-    }
+		// copies all fields from 'product' to 'existingProduct'
+		// We can specify fields at the end to ignore.
+		BeanUtils.copyProperties(product, existingProduct, "id");
+		return productRepository.save(existingProduct);
+	}
 
-    public Product getProductById(String id) {
-        try {
-            Integer numericId = Integer.valueOf(id);
-            return productRepository.findById(numericId)
-                    .orElseThrow(() -> new ResourceNotFoundException("Product not found with ID: " + numericId));
-        } catch (NumberFormatException e) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "ID must be a numeric value.");
-        }
-    }
+	public Product getProductById(String id) {
+		try {
+			Integer numericId = Integer.valueOf(id);
+			return productRepository.findById(numericId)
+					.orElseThrow(() -> new ResourceNotFoundException("Product not found with ID: " + numericId));
+		} catch (NumberFormatException e) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "ID must be a numeric value.");
+		}
+	}
 }
