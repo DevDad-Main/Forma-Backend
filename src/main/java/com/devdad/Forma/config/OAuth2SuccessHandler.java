ba7b7064400e7bdcc86fc659e4ac2a4b606712fa
Oliver Metz <softwaredevdad@gmail.com>
2026-05-09 18:34:36 +0200

    update: OAuth2SuccessHandler.
    - change redirect url to include the token, for the frontend to use and
    potentially fix auth issues.

--- src/main/java/com/devdad/Forma/config/OAuth2SuccessHandler.java
+++ src/main/java/com/devdad/Forma/config/OAuth2SuccessHandler.java
@@ -141,7 +141,8 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
 		// Redirect to this url as the frontend listens to this URI and then navigates
 		// the users to their profile.
 		// response.sendRedirect("http://localhost:5173/login/oauth2/code/google");
-		response.sendRedirect("https://devdad-forma.vercel.app/login/oauth2/code/google");
+		// response.sendRedirect("https://devdad-forma.vercel.app/login/oauth2/code/google");
+		response.sendRedirect("https://devdad-forma.vercel.app/login/oauth2/code/google?token=" + token);
 
 		// // Return JSON instead of redirecting
 		// // This lets the frontend decide where to go next
